eab36dac9b8d49f4de0b4b4bd03315d6153ba0ea
Glenn <g.todd@internet.co.nz>
2025-09-07 17:08:47 +1200

    Got the python debugger working with ChatGBT help

--- lua/plugins/enhancements/nvim-debugging.lua
+++ lua/plugins/enhancements/nvim-debugging.lua
@@ -1,75 +1,56 @@
--- Docs at https://github.com/mfussenegger/nvim-dap-python are useful.
-return {
-  -- keep-sorted start block=yes
+-- file ~/.config/nvim/lua/plugins/enhancements/nvim-debugging.lua
 
+return {
   {
     "mfussenegger/nvim-dap",
     lazy = true,
-    -- Copied from LazyVim/lua/lazyvim/plugins/extras/dap/core.lua and
-    -- modified.
     keys = {
-      {
-        "<leader>db",
-        function() require("dap").toggle_breakpoint() end,
-        desc = "Toggle Breakpoint"
-      },
-
-      {
-        "<leader>dc",
-        function() require("dap").continue() end,
-        desc = "Continue"
-      },
-
-      {
-        "<leader>dC",
-        function() require("dap").run_to_cursor() end,
-        desc = "Run to Cursor"
-      },
-
-      {
-        "<leader>dT",
-        function() require("dap").terminate() end,
-        desc = "Terminate"
-      },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Debug: Start/Continue" },
+      { "<leader>ds", function() require("dap").step_into() end, desc = "Debug: Step Into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Debug: Step Over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Debug: Step Out" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>dT", function() require("dap").terminate() end, desc = "Terminate" },
     },
     dependencies = {
       {
         "igorlfs/nvim-dap-view",
-        -- Alternative UI.  https://igorlfs.github.io/nvim-dap-view/keymaps
-        ---@module 'dap-view'
-        ---@type dapview.Config
-        opts = {
-          -- Automatically toggle dap-view windows when starting/stopping a
-          -- session by pressing continue.
-          auto_toggle = true,
-        },
+        opts = { auto_toggle = true },
       },
-    }
+    },
   },
+
   {
     "rcarriga/nvim-dap-ui",
-    config = true,
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+
+      -- Auto open/close dap-ui
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
     keys = {
       {
         "<leader>du",
-        function()
-          require("dapui").toggle({})
-        end,
-        desc = "Dap UI"
+        function() require("dapui").toggle({}) end,
+        desc = "Debug: Toggle UI",
       },
     },
     dependencies = {
-      -- keep-sorted start block=yes
       {
         "jay-babu/mason-nvim-dap.nvim",
-        ---@type MasonNvimDapSettings
         opts = {
-          -- This line is essential to making automatic installation work
-          -- :exploding-brain
           handlers = {},
           automatic_installation = false,
-          -- DAP servers: these will be installed by mason-tool-installer.nvim
-          -- for consistency.
           ensure_installed = {},
         },
         dependencies = {
@@ -81,28 +62,65 @@ return {
         "mfussenegger/nvim-dap-python",
         lazy = true,
         config = function()
-          -- debugpy is installed in my standard Python virtualenv so that it's
-          -- available with all the other modules.
-          require("dap-python").setup("python3")
+          local dap = require("dap")
+          require("dap-python").setup("python")
+
+          dap.configurations.python = {
+            {
+              type = "python",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              pythonPath = function()
+                local venv_path = os.getenv("VIRTUAL_ENV")
+                if venv_path then
+                  return venv_path .. "/bin/python"
+                end
+                return "/usr/local/bin/python"
+              end,
+            },
+          }
         end,
-        -- Consider the mappings at
-        -- https://github.com/mfussenegger/nvim-dap-python?tab=readme-ov-file#mappings
-        dependencies = {
-          "mfussenegger/nvim-dap",
-        },
+        dependencies = { "mfussenegger/nvim-dap" },
       },
-      {
-        "nvim-neotest/nvim-nio",
-      },
+      { "nvim-neotest/nvim-nio" },
       {
         "theHamsta/nvim-dap-virtual-text",
-        config = true,
-        dependencies = {
-          "mfussenegger/nvim-dap",
-        },
+        config = true, -- inline virtual text
+        dependencies = { "mfussenegger/nvim-dap" },
       },
-      -- keep-sorted end
     },
   },
-  -- keep-sorted end
+
+  -- 🔥 Lualine integration for dap status + breakpoints
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "mfussenegger/nvim-dap" },
+    opts = function(_, opts)
+      local dap = require("dap")
+
+      local function dap_status()
+        return dap.status()
+      end
+
+      local function dap_breakpoints()
+        local breakpoints = require("dap.breakpoints").get()
+        local count = 0
+        for _, buf_bps in pairs(breakpoints) do
+          count = count + #buf_bps
+        end
+        if count > 0 then
+          return " " .. count
+        end
+        return ""
+      end
+
+      opts.sections = opts.sections or {}
+      opts.sections.lualine_x = opts.sections.lualine_x or {}
+      table.insert(opts.sections.lualine_x, dap_status)
+      table.insert(opts.sections.lualine_x, dap_breakpoints)
+
+      return opts
+    end,
+  },
 }
